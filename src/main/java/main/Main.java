c0c357f69950f2b8d54ea32964a7f0e72ab0d577
SwiftFalcon <korolyov.k@gmail.com>
2018-12-05 19:29:07 +0300

    Add new packages and rewrite class Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,24 @@
+package main;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        Frontend frontend = new Frontend();
-
-        Server server = new Server(8080);
+        AllRequestsServlet frontend = new AllRequestsServlet();
 
         //creating servlet-container and set what it'll process
         ServletContextHandler context =
                 new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+        Server server = new Server(8080);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+        //context.addServlet(new ServletHolder(frontend), "/authform");
     }
 
 }
